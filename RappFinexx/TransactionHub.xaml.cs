1691cf9729db1b0ea9ceb04e0fa4be9bfd57310c
Tyler Hodgkin <tjh2389@hotmail.com>
2018-12-07 16:52:59 -0700

    minor changes

---
+++ RappFinexx/TransactionHub.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RappFinexx
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void StackPanel_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Border sp = sender as Border;
+            DoubleAnimation db = new DoubleAnimation();
+            //db.From = 12;
+            db.To = 150;
+            db.Duration = TimeSpan.FromSeconds(0.5);
+            db.AutoReverse = false;
+            db.RepeatBehavior = new RepeatBehavior(1);
+            sp.BeginAnimation(StackPanel.HeightProperty, db);
+        }
+
+        private void StackPanel_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Border sp = sender as Border;
+            DoubleAnimation db = new DoubleAnimation();
+            //db.From = 12;
+            db.To = 12;
+            db.Duration = TimeSpan.FromSeconds(0.5);
+            db.AutoReverse = false;
+            db.RepeatBehavior = new RepeatBehavior(1);
+            sp.BeginAnimation(StackPanel.HeightProperty, db);
+        }
+    }
+}
